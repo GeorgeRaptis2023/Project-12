b39e0206a0872edf275312e006b45ff1334133d4
George Raptis <g.raptis2023@gmail.com>
2023-12-19 22:51:09 +0200

    ok

--- example.py
+++ example.py
@@ -4,70 +4,148 @@ class App():
     def __init__(self, root):
         self.root=root
         self.root.title("Ai Image Recognition")
-        root.geometry("450x450")
+        root.geometry("500x500")
+
         self.frame=tkinter.Frame(root)
         self.frame.grid(row=0,column=0)
+
         self.title=tkinter.Label(self.frame,text="Compare")
         self.title.grid(row=0,column=0)
-        self.resultl=tkinter.Label(self.frame,text="result:")
-        self.resultl.grid(row=2,column=0)
-        self.result=tkinter.Label(self.frame,text="")
-        self.result.grid(row=2,column=1)
-        self.face=tkinter.Label(self.frame)
-        self.face.grid(row=2,column=2)
+        
         self.button=tkinter.Button(self.frame,text="click",command=self.compare)
         self.button.grid(row=0,column=1)
-        self.tolerance = tkinter.Scale(self.frame,from_=1, to=100, tickinterval=5, orient="horizontal")
-        self.tolerance.grid(row=0,column=2)
-        self.numj = tkinter.Scale(self.frame,from_=1, to=100, tickinterval=5, orient="horizontal")
-        self.numj.grid(row=0,column=3)
         
-        self.listbox1 = tkinter.Listbox(self.frame,height=3,exportselection=False) 
-        self.listbox1.grid(row=1,column=2)
-        self.listbox1.insert(1,"large")
-        self.listbox1.insert(2,"small")
-        self.listbox2 = tkinter.Listbox(self.frame,heigh=3,exportselection=False) 
-        self.listbox2.grid(row=1,column=3)
-        self.listbox2.insert(1,"hog")
-        self.listbox2.insert(2,"cnn")
+        self.comparison=tkinter.Label(self.frame,text="")
+        self.comparison.grid(row=12,column=0,columnspan=8)
+
+        self.result1=tkinter.Label(self.frame,text="")
+        self.result1.grid(row=3,column=0,columnspan=4)
+
+        self.face1=tkinter.Label(self.frame)
+        self.face1.grid(row=4,column=0,columnspan=2,rowspan=2)
+
+        self.face2=tkinter.Label(self.frame)
+        self.face2.grid(row=4,column=2,columnspan=2,rowspan=2)
+
+        self.tolerance1 = tkinter.Scale(self.frame,from_=1, to=100, tickinterval=5, orient="horizontal")
+        self.tolerance1.grid(row=0,column=2)
+
+        self.option_numjitters1 = tkinter.Scale(self.frame,from_=1, to=20, tickinterval=5,orient="horizontal")
+        self.option_numjitters1.grid(row=0,column=3)
         
-    def compare(self):
-        t_start=time.process_time()
-        try:
-            model1=self.listbox1.curselection()[0]
-            model2=self.listbox2.curselection()[0]
-            tolerancecomp=self.tolerance.get()/100
-            numjitters=self.numj.get()
-            
-            if model1:model1="small"
-            else:model1="large"
-            if model2:model2="cnn"
-            else:model2="hog"
-            print(model1,model2,numjitters,tolerancecomp)
-            filelocation = tkinter.filedialog.askopenfilename()
-            known_image = face_recognition.load_image_file(filelocation) 
-            filelocation = tkinter.filedialog.askopenfilename()
-            unknown_image = face_recognition.load_image_file(filelocation)
-            me = face_recognition.face_encodings(known_image,model=model1,num_jitters=numjitters)[0] 
-            face_locations=face_recognition.face_locations(unknown_image,model=model2) 
-            unknown_encoding = face_recognition.face_encodings(unknown_image,model=model1,num_jitters=numjitters)[0] 
-            result=face_recognition.compare_faces([me], unknown_encoding,tolerance=tolerancecomp) 
-            distance=face_recognition.api.face_distance([me], unknown_encoding) 
+        self.option_size1 = tkinter.Listbox(self.frame,height=3,exportselection=False) 
+        self.option_size1.grid(row=2,column=1)
+        self.option_size1.insert(1,"large")
+        self.option_size1.insert(2,"small")
+        self.option_type1 = tkinter.Listbox(self.frame,heigh=3,exportselection=False) 
+        self.option_type1.grid(row=2,column=2)
+        self.option_type1.insert(1,"hog")
+        self.option_type1.insert(2,"cnn")
+
+        self.result2=tkinter.Label(self.frame,text="")
+        self.result2.grid(row=9,column=0,columnspan=4)
+
+        self.face3=tkinter.Label(self.frame)
+        self.face3.grid(row=10,column=0,columnspan=2,rowspan=2)
+        
+        self.face4=tkinter.Label(self.frame)
+        self.face4.grid(row=10,column=2,columnspan=2,rowspan=2)
+
+
+        self.tolerance2 = tkinter.Scale(self.frame,from_=1, to=100, tickinterval=5, orient="horizontal")
+        self.tolerance2.grid(row=6,column=2)
+
+        self.option_numjitters2 = tkinter.Scale(self.frame,from_=1, to=20, tickinterval=5,orient="horizontal")
+        self.option_numjitters2.grid(row=6,column=3)
+        
+        self.option_size2 = tkinter.Listbox(self.frame,height=3,exportselection=False) 
+        self.option_size2.grid(row=8,column=1)
+        self.option_size2.insert(1,"large")
+        self.option_size2.insert(2,"small")
+        
+        self.option_type2 = tkinter.Listbox(self.frame,heigh=3,exportselection=False) 
+        self.option_type2.grid(row=8,column=2)
+        self.option_type2.insert(1,"hog")
+        self.option_type2.insert(2,"cnn")
+
+    def recognize(self,encoding_size,model_type,tolerance,numjitters,filelocation1,filelocation2,text):
+            if encoding_size:encoding_size="small"
+            else:encoding_size="large"
+            if model_type:model_type="cnn"
+            else:model_type="hog"
+            t_start=time.process_time()
+
+            first_image = face_recognition.load_image_file(filelocation1) 
+            second_image = face_recognition.load_image_file(filelocation2)
+
+            first_encoding = face_recognition.face_encodings(first_image,model=encoding_size,num_jitters=numjitters)[0] 
+            second_encoding = face_recognition.face_encodings(second_image,model=encoding_size,num_jitters=numjitters)[0] 
+
+            result=face_recognition.compare_faces([first_encoding], second_encoding,tolerance=tolerance) 
+            distance=face_recognition.api.face_distance([first_encoding], second_encoding) 
+            face_locations1=face_recognition.face_locations(first_image,model=model_type) 
+            face_locations2=face_recognition.face_locations(second_image,model=model_type)
             t_process_ms=1000*(time.process_time()-t_start)       
             if result[0]:
-                answer="Known"
+                answer="Recognized"
             else:
-                answer="Unknown"
-            self.result.configure(text=f"{answer} in {t_process_ms:.2f} ms with distance {distance[0]:.2f}")        
-            top, right, bottom, left = face_locations[0]
-            face_image = unknown_image[top:bottom, left:right]
+                answer="Failed"
+            text.configure(text=f"Result: {answer} in {t_process_ms:.2f} ms with distance {distance[0]:.2f}")        
+            top, right, bottom, left = face_locations1[0]
+            face_image = first_image[top:bottom, left:right]
             pil_image = Image.fromarray(face_image)
-            pil_image=pil_image.resize((80,80))    
-            self.image_tk=ImageTk.PhotoImage(pil_image)        
-            self.face['image']=self.image_tk
+            pil_image=pil_image.resize((75,75))       
+            top, right, bottom, left = face_locations2[0]
+            face_image2 = second_image[top:bottom, left:right]
+            pil_image2 = Image.fromarray(face_image2)
+            pil_image2=pil_image2.resize((75,75))    
+            return ImageTk.PhotoImage(pil_image),ImageTk.PhotoImage(pil_image2),t_process_ms,distance
+
+        
+        
+    def compare(self):
+        try:
+            filelocation1 = tkinter.filedialog.askopenfilename()
+            filelocation2 = tkinter.filedialog.askopenfilename()
+            encoding_size=self.option_size1.curselection()[0]
+            model_type=self.option_type1.curselection()[0]
+            tolerance=self.tolerance1.get()/100
+            numjitters=self.option_numjitters1.get()
         except:
-            print("Wrong selection")
+            print("Wrong Selection")
             return
+        try:
+
+            self.image1,self.image2,time1,distance1=self.recognize(encoding_size,model_type,tolerance,numjitters,filelocation1,filelocation2,self.result1)
+            self.face1['image'],self.face2['image']=self.image1,self.image2
+        except:
+            print('Failed')
+            return
+        try:
+            filelocation1 = tkinter.filedialog.askopenfilename()
+            filelocation2 = tkinter.filedialog.askopenfilename()
+            encoding_size=self.option_size2.curselection()[0]
+            model_type=self.option_type2.curselection()[0]
+            tolerance=self.tolerance2.get()/100
+            numjitters=self.option_numjitters2.get()
+        except:
+            print("Wrong Selection")
+            return
+        try:
+            self.image3,self.image4,time2,distance2=self.recognize(encoding_size,model_type,tolerance,numjitters,filelocation1,filelocation2,self.result2)
+            self.face3['image'],self.face4['image']=self.image3,self.image4
+        except:
+            print('Failed')
+            return
+        comptext=""
+        if(abs(time1-time2)<0.01):comptext+=f"Both processes took about the same time ."
+        elif(time1>time2):comptext+=f"The second process was faster by {time1-time2} ms ."
+        else:comptext+=f"The first process was faster by {time2-time1} ms ."
+        if(abs(distance1-distance2)<0.01):comptext+=f"Both processes found about the same distance"
+        elif(distance2>distance1):comptext+=f"The first process found a smaller distance by {distance2-distance1} "
+        else:comptext+=f"The second process  process found a smaller distance by {distance1[0]-distance2[0]}"
+        self.comparison.configure(text=comptext)
+
         
     
         
